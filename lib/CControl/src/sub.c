cd6ef48def17c45a4bfa25438afed1ac4e85c2aa
Izsó András <izso.andras@simonyi.bme.hu>
2021-10-14 00:51:12 +0200

    Fixed type in sub.c

--- lib/CControl/src/sub.c
+++ lib/CControl/src/sub.c
@@ -13,7 +13,7 @@
  * B [row*col]
  */
 void sub(float A[], float B[], float C[], uint16_t length){
-    for(uint16_t i=0; i < length, i++){
+    for(uint16_t i=0; i < length; i++){
         C[i] = A[i] - B[i];
     }
 }
